e3f1a519b0fea5e3d1e4b0cc649aebb4435b7d64
Stelios Klapsis <Steliosklapsis@outlook.com>
2023-01-15 01:38:33 -0500

    Updated Tool System / Corrected Bugs

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,22 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Experimental.Playables;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
     private ToolType? m_CurrentTool = null;
+    private TMP_Text m_ToolPrompt;
 
-    void OnCollisionStay2D(Collision2D col)
+    private bool m_InToolRange = false;
+    private Tool m_ToolRef;
+
+    void Awake()
     {
-        if (!Input.GetKeyDown(KeyCode.F))
-        {
-            return;
-        }
+        m_ToolPrompt = GetComponentInChildren<TMP_Text>();
+    }
 
-        if (TryGetComponent(out Tool tool))
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
         {
-            m_CurrentTool = tool.PickupTool();
+            if (m_InToolRange)
+            {
+                m_CurrentTool = m_ToolRef.ReturnToolType();
+                m_ToolRef.DestroyTool();
+                Debug.Log(m_CurrentTool.ToString());
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.TryGetComponent(out Tool tool))
+        {
+            //sets flag / stores ref
+            m_InToolRange = true;
+            m_ToolRef = tool;
+
+            //Updates the tool prompt above the player
+            m_ToolPrompt.enabled = true;
+            m_ToolPrompt.text = $"Press F to pickup the {tool.ReturnToolType().ToString()}.";
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.TryGetComponent(out Tool tool))
+        {
+            //Sets flag
+            m_InToolRange = false;
+
+            //Updates the tool prompt above the player
+            m_ToolPrompt.text = "";
+            m_ToolPrompt.enabled = false;
         }
     }
 
