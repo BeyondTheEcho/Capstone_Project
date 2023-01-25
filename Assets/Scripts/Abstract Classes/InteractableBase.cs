189148afad1d022f26fecd97bb985577550aa06a
Stelios Klapsis <Steliosklapsis@outlook.com>
2023-01-25 01:02:55 -0500

    Added documentation for complex / verbose functions

--- Assets/Scripts/Abstract Classes/InteractableBase.cs
+++ Assets/Scripts/Abstract Classes/InteractableBase.cs
@@ -7,11 +7,17 @@ public abstract class InteractableBase : MonoBehaviour
     public abstract void OnInteract(Player player);
     public abstract string ReturnTextPrompt();
 
+    /// <summary>
+    /// Stores an instance of the interactable object in the Interactables Manager
+    /// </summary>
     public void StoreRef()
     {
         InteractablesManager.s_Instance.AddInteractable(this);
     }
 
+    /// <summary>
+    /// Removes this instance of the interactable object in the Interactables Manager
+    /// </summary>
     public void DeleteRef()
     {
         InteractablesManager.s_Instance.RemoveInteractable(this);
