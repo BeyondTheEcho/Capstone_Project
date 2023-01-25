45afdcfd37f9400b3b8c57f6e66dd1c8b3f786f7
Stelios Klapsis <Steliosklapsis@outlook.com>
2023-01-24 23:47:05 -0500

    Added ability to add items to belts

--- Assets/Scripts/Objects/Belts.cs
+++ Assets/Scripts/Objects/Belts.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
-public class Belts : MonoBehaviour
+public class Belts : InteractableBase
 {
+    public Transform m_NextWaypoint;
+
+    void Awake()
+    {
+        StoreRef();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +22,40 @@ public class Belts : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void PlaceItemOnBelt(Player player)
+    {
+        MonoBehaviour mono = (MonoBehaviour) player.m_HeldItem;
+
+        InteractableBase interactable = (InteractableBase) player.m_HeldItem;
+
+        if (mono.TryGetComponent(out Transform itemTransform))
+        {
+            itemTransform.parent = null;
+
+            itemTransform.position = gameObject.transform.position;
+
+            player.m_HeldItem = null;
+
+            itemTransform.gameObject.SetActive(true);
+
+            interactable.StoreRef();
+        }
+    }
+
+    public override void OnInteract(Player player)
+    {
+        // Does Nothing For Now
+    }
+
+    public override string ReturnTextPrompt()
+    {
+        return $"Press 'F' to add or remove an object from the belt";
+    }
+
+    private void OnDestroy()
+    {
+        DeleteRef();
     }
 }
