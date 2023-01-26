f158a638e71f5ee4b4b45b432522a8b41cf98543
Joshua Da Silva <joshua.dasilva@student.sl.on.ca>
2023-01-25 20:21:18 -0500

    Fixed a spelling issue in Menu

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,7 +6,6 @@ public class PlayerController : MonoBehaviour
 {
     //Serialized Vars
     [SerializeField] private float m_Speed = 10.0f;
-    public PlayerNumber playerNumber;
 
     //Refs
     private Rigidbody2D m_Rigidbody;
@@ -25,28 +24,9 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 velocity;
 
-        if (playerNumber == PlayerNumber.PlayerOne)
-        {
-            velocity.x = Input.GetAxisRaw("Horizontal");
-            velocity.y = Input.GetAxisRaw("Vertical");
+        velocity.x = Input.GetAxisRaw("Horizontal");
+        velocity.y = Input.GetAxisRaw("Vertical");
 
-            m_Rigidbody.MovePosition(m_Rigidbody.position + velocity * m_Speed * Time.fixedDeltaTime);
-        }
-
-        if (playerNumber == PlayerNumber.PlayerTwo)
-        {
-            velocity.x = Input.GetAxisRaw("Horizontal2");
-            velocity.y = Input.GetAxisRaw("Vertical2");
-
-            m_Rigidbody.MovePosition(m_Rigidbody.position + velocity * m_Speed * Time.fixedDeltaTime);
-        }
-    }
-
-    public enum PlayerNumber
-    {
-        PlayerOne,
-        PlayerTwo,
-        PlayerThree,
-        PlayerFour
+        m_Rigidbody.MovePosition(m_Rigidbody.position + velocity * m_Speed * Time.fixedDeltaTime);
     }
 }
