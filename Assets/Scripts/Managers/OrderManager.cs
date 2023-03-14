e634b4dbe97628ec9e53b70c0f3b1fa78712e451
Stelios Klapsis <Steliosklapsis@outlook.com>
2023-03-14 18:12:16 -0400

    Fixed Merge Issues + Bugs

--- Assets/Scripts/Managers/OrderManager.cs
+++ Assets/Scripts/Managers/OrderManager.cs
@@ -17,12 +17,10 @@ public class OrderManager : MonoBehaviour
     //Order Vars
     [Header("Order Settings")]
     [SerializeField] private Sprite[] m_OrderItems;
-    [SerializeField] private GameObject m_OrderSpawnBelt;
     private int m_BaseOrderSize = 10;
     private int m_CurrentOrderSize = 0;
     private int m_MaxOrderSize = 0;
     private int m_CurrentOrderItem;
-    private float m_OrderSpawnDelay = 3f;
     private int m_TotalOrderSize = 0;
 
     //Chaos Vars
@@ -53,7 +51,6 @@ public class OrderManager : MonoBehaviour
         }
     }
 
-
     void Start()
     {
         m_ChaosCoroutine = StartCoroutine(IncrementChaos());
@@ -80,8 +77,6 @@ public class OrderManager : MonoBehaviour
         m_CurrentOrderItem = Random.Range(0, orderItemMax);
 
         m_TotalOrderSize = m_CurrentOrderSize;
-
-        StartCoroutine(SpawnOrder(m_CurrentOrderSize));
     }
 
     public void SubtractOrderItem()
@@ -98,7 +93,7 @@ public class OrderManager : MonoBehaviour
     {
         m_ChaosText.text = $"Chaos: {m_Chaos}";
         m_OrderText.text = $"Order Size: {m_CurrentOrderSize}";
-        m_PopupOrder.sprite = m_OrderItems[0].GetComponent<SpriteRenderer>().sprite;
+        m_PopupOrder.sprite = m_OrderItems[m_CurrentOrderItem];
         m_ItemText.text = $"X{m_CurrentOrderSize}";
     }
 
