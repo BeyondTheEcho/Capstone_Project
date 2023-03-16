3dc8f5d544ebf754d4372f327cc778038af658c5
CHUNG-HAO LI <91105935+LiChung-Hao@users.noreply.github.com>
2023-03-16 09:48:21 -0400

    mouse added

---
+++ Assets/Animations/MousePops/MousPops.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousPops : MonoBehaviour
+{
+    [SerializeField] GameObject m_mouse;
+    public static bool m_mouseInScene = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GenMouse();
+    }
+
+    void GenMouse()
+    {
+        if (!m_mouseInScene)
+        {
+            Vector3 mousePos = new Vector3(Random.Range(20, -20.2f), Random.Range(7, -8.5f), 0);
+            Instantiate(m_mouse, mousePos, Quaternion.Euler(Random.Range(0, 30), 0, 0));
+            m_mouseInScene = true;
+        }
+
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            m_mouseInScene = false;
+            Destroy(m_mouse);
+        }
+    }
+}
