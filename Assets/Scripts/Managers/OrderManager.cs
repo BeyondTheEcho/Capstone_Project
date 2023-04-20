e6810939d0cc9eee45f2ad2c1525782c33830c9c
Dragonfactor <dorian.scrimshaw@student.sl.on.ca>
2023-04-20 08:43:43 -0400

    Changes and bug fix

--- Assets/Scripts/Managers/OrderManager.cs
+++ Assets/Scripts/Managers/OrderManager.cs
@@ -136,7 +136,10 @@ public class OrderManager : MonoBehaviour
             if (item == order)
             {
                 Destroy(item.gameObject);
-                GameManager.s_Instance.UpdateScore(m_OrderValue);
+                if (Order.m_TimeRemaining > 0)
+                {
+                    GameManager.s_Instance.UpdateScore(m_OrderValue);
+                }
                 
                 return;
             }
